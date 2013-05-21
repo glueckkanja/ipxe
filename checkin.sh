b95c40bf8b629baa37a89e984513bce6395ab974
cekageka <christian.kanja@glueckkanja.com>
2013-05-21 17:57:01 +0200

    Check In Script

--- checkin.sh
+++ checkin.sh
@@ -1,15 +1,17 @@
+echo -e "Checkin Script...\n"
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update gh-pages\n"
+  echo -e "Starting to update \n"
 
-  cp -R coverage $HOME/coverage
+  cp -R bin $HOME/bin
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Uko/Rubidium-WHOIS.git  gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Uko/Rubidium-WHOIS.git  gh-pages 
+  #> /dev/null
 
   cd gh-pages
-  cp -Rf $HOME/coverage/* .
+  cp -Rf $HOME/bin/* .
 
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
