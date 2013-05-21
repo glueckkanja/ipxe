5ad3e09c0f41620993945033fb76d0200bc704d1
cekageka <christian.kanja@glueckkanja.com>
2013-05-21 18:53:53 +0200

    bash

--- checkin.sh
+++ checkin.sh
@@ -1,20 +1,24 @@
 #if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update \n"
 
-  cp -R bin $HOME/bin
+  #cp -R bin $HOME/bin
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/glueckkanja/ipxe.git  gh-pages 
-  #> /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/glueckkanja/ipxe.git gh-pages -v
+
+  ls -la
+  
+
+#> /dev/null
 
   cd gh-pages
-  cp -Rf $HOME/bin/* .
+  cp -Rf $HOME/srv/bin/* .
 
-  git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages 
+  git add -f . -v
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages" -v
+  git push -fq origin gh-pages -v
 
   echo -e "Done magic with coverage\n"
   #fi
