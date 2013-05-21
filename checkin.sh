fa2044b49c0802eea33315f84c4948ab88353e6a
cekageka <christian.kanja@glueckkanja.com>
2013-05-21 20:48:26 +0300

    checkin update

--- checkin.sh
+++ checkin.sh
@@ -1,24 +1,18 @@
-#if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update \n"
-
-  #cp -R bin $HOME/bin
+  echo -e "Starting to update gh-pages\n"
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/glueckkanja/ipxe.git gh-pages -v
+  #> /dev/null
 
-  ls -la
-  ls -la build/glueckkanja
-
-#> /dev/null
+  #ls -la build/glueckkanja/ipxe
 
   cd gh-pages
-  cp -Rf $HOME/build/glueckkanja/ipxe/src/bin/* .
+  cp -Rf $HOME/build/glueckkanja/ipxe/src/bin .
 
   git add -f . -v
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages" -v
   git push -fq origin gh-pages -v
 
-  echo -e "Done magic with coverage\n"
-  #fi
+  echo -e "Done.\n"
