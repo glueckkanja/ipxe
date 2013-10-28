c69e10091573d96b5be42de501424ed243aca3cd
Travis <travis@travis-ci.org>
2013-10-28 13:38:47 +0000

    Travis build 72 pushed to gh-pages

---
+++ bin/deps/arch/i386/core/cachedhcp.c.d
@@ -0,0 +1,111 @@
+cachedhcp_DEPS += arch/i386/core/cachedhcp.c include/compiler.h \
+ arch/i386/include/bits/compiler.h include/stdint.h \
+ arch/i386/include/bits/stdint.h include/stdlib.h include/assert.h \
+ include/ipxe/dhcppkt.h include/ipxe/dhcp.h include/stdarg.h \
+ include/ipxe/in.h include/ipxe/socket.h include/ipxe/tables.h \
+ include/ipxe/list.h include/stddef.h include/ipxe/refcnt.h \
+ include/ipxe/uuid.h include/byteswap.h include/endian.h \
+ arch/i386/include/bits/endian.h arch/i386/include/bits/byteswap.h \
+ include/little_bswap.h include/ipxe/netdevice.h include/ipxe/settings.h \
+ include/ipxe/uaccess.h include/string.h arch/x86/include/bits/string.h \
+ include/ipxe/api.h config/ioapi.h config/defaults.h \
+ config/defaults/pcbios.h config/local/ioapi.h \
+ include/ipxe/efi/efi_uaccess.h include/ipxe/linux/linux_uaccess.h \
+ arch/i386/include/bits/uaccess.h arch/i386/include/librm.h \
+ include/ipxe/dhcpopts.h include/ipxe/init.h arch/i386/include/realmode.h \
+ arch/i386/include/registers.h arch/i386/include/pxe_api.h \
+ arch/i386/include/pxe_types.h include/errno.h \
+ arch/i386/include/ipxe/errno/pcbios.h arch/i386/include/pxe_error.h \
+ include/ipxe/errfile.h arch/x86/include/bits/errfile.h
+
+include/compiler.h:
+
+arch/i386/include/bits/compiler.h:
+
+include/stdint.h:
+
+arch/i386/include/bits/stdint.h:
+
+include/stdlib.h:
+
+include/assert.h:
+
+include/ipxe/dhcppkt.h:
+
+include/ipxe/dhcp.h:
+
+include/stdarg.h:
+
+include/ipxe/in.h:
+
+include/ipxe/socket.h:
+
+include/ipxe/tables.h:
+
+include/ipxe/list.h:
+
+include/stddef.h:
+
+include/ipxe/refcnt.h:
+
+include/ipxe/uuid.h:
+
+include/byteswap.h:
+
+include/endian.h:
+
+arch/i386/include/bits/endian.h:
+
+arch/i386/include/bits/byteswap.h:
+
+include/little_bswap.h:
+
+include/ipxe/netdevice.h:
+
+include/ipxe/settings.h:
+
+include/ipxe/uaccess.h:
+
+include/string.h:
+
+arch/x86/include/bits/string.h:
+
+include/ipxe/api.h:
+
+config/ioapi.h:
+
+config/defaults.h:
+
+config/defaults/pcbios.h:
+
+config/local/ioapi.h:
+
+include/ipxe/efi/efi_uaccess.h:
+
+include/ipxe/linux/linux_uaccess.h:
+
+arch/i386/include/bits/uaccess.h:
+
+arch/i386/include/librm.h:
+
+include/ipxe/dhcpopts.h:
+
+include/ipxe/init.h:
+
+arch/i386/include/realmode.h:
+
+arch/i386/include/registers.h:
+
+arch/i386/include/pxe_api.h:
+
+arch/i386/include/pxe_types.h:
+
+include/errno.h:
+
+arch/i386/include/ipxe/errno/pcbios.h:
+
+arch/i386/include/pxe_error.h:
+
+include/ipxe/errfile.h:
+
+arch/x86/include/bits/errfile.h:
